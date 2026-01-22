d72c44908b4bb7695719adb470515219f0a5c1f4
Mennovos <mennodvos@gmail.com>
2026-01-22 15:40:31 +0100

    Bwet

--- Card Game/Assets/Scripts/Buttons functions/Healss.cs
+++ Card Game/Assets/Scripts/Buttons functions/Healss.cs
@@ -18,7 +18,7 @@ public class Healss : Button
     }
     private void OnMouseOver()
     {
-        Orginelscale = new Vector3(1.3f, 1.3f, 1.3f);
+        transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
     }
     private void OnMouseExit()
     {
