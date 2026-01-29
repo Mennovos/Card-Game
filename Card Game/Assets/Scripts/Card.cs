ee5f172d6cfce5b3e768251f638be824e6eb16a0
Mennovos <mennodvos@gmail.com>
2026-01-29 11:54:01 +0100

    Update

--- Card Game/Assets/Scripts/Card.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Card", menuName = "Scriptable Objects/Card")]
-public class Card : ScriptableObject
-{
-    public new string CardName;
-    public Image Artwork;
-}
